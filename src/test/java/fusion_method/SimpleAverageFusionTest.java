a7d6c7cdea66dd9d1b3ab78ab45f3866eeae68c8
Arthur Henning <h_arthur_12@yahoo.com>
2014-04-02 23:24:21 +0300

    Created unit tests for fusion methods. Issue #9 Haar and Laplacian NullPointerException.

---
+++ src/test/java/fusion_method/SimpleAverageFusionTest.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2014 Arthur Henning.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package fusion_method;
+
+import ij.ImagePlus;
+import junit.framework.TestCase;
+
+/**
+ *
+ * @author Arthur Henning
+ */
+public class SimpleAverageFusionTest extends TestCase {
+
+    public SimpleAverageFusionTest(String testName) {
+        super(testName);
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
+
+    /**
+     * Test of fuse method, of class SimpleAverageFusion.
+     */
+    public void testFuse() {
+        System.out.println("fuse");
+        ImagePlus image1 = new ImagePlus("F:\\test_images\\abdomen_1.jpg");
+        image1.show();
+        ImagePlus image2 = new ImagePlus("F:\\test_images\\abdomen_2.jpg");
+        image2.show();
+        SimpleAverageFusion instance = new SimpleAverageFusion();
+        ImagePlus expResult = null;
+        ImagePlus result = instance.fuse(image1, image2);
+        result.show();
+        assertEquals(expResult, result);
+    }
+
+}
